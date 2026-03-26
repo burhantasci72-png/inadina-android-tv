40c51c003d552fbab26aa7db206c48d563d37e03
burhantasci72-png <burhantasci72@gmail.com>
2026-03-26 20:35:40 +0300

    Create kanallar.js

---
+++ api/kanallar.js
@@ -0,0 +1,3 @@
+export default function handler(req, res) {
+    res.status(200).json({ "TEST":[{ "number":1, "name":"TRT 1", "logo":"", "url":"https://tv-trt1.medya.trt.com.tr/master.m3u8" }] });
+}
